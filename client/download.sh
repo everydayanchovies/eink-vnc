b8858344854bcdb565318617348c175c1fbe1cee
LordEagle <r.wermelinger@gmail.com>
2024-10-16 22:19:20 +0200

    fix plato download uri

--- client/download.sh
+++ client/download.sh
@@ -6,7 +6,8 @@ archive="plato-0.9.30.zip"
 if ! [ -e "$archive" ]; then
     info_url="https://api.github.com/repos/baskerville/plato/releases/tags/${version}"
     echo "Downloading ${archive}."
-    release_url=$(wget -q -O - "$info_url" | jq -r ".assets[] | select(.name == \"$archive\").browser_download_url")
+    #release_url=$(wget -q -O - "$info_url" | jq -r ".assets[] | select(.name == \"$archive\").browser_download_url")
+    release_url=https://github.com/baskerville/plato/releases/download/0.9.30/plato-0.9.30.zip
     wget -q --show-progress "$release_url"
 fi
 unzip "$archive" "$@"
